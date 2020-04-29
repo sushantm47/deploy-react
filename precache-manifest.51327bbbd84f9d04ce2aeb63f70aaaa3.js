6ad3ccf1db354994d2e9213db0115d7b76063557
Sushant <vettel@pop-os.localdomain>
2020-04-29 13:00:12 +0530

    Updates

---
+++ precache-manifest.51327bbbd84f9d04ce2aeb63f70aaaa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66650146a1e5c7c9c27c91985279fb19",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "3dc411d0712e61b1bb76",
+    "url": "/deploy-react/static/css/main.810becba.chunk.css"
+  },
+  {
+    "revision": "3a1a6764abc208a64de4",
+    "url": "/deploy-react/static/js/2.44175c02.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/deploy-react/static/js/2.44175c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dc411d0712e61b1bb76",
+    "url": "/deploy-react/static/js/main.23ec39ff.chunk.js"
+  },
+  {
+    "revision": "887b96e372b6ba34939c",
+    "url": "/deploy-react/static/js/runtime-main.de2f369f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/deploy-react/static/media/image.d7265326.png"
+  }
+]);
